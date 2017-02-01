663682bc6598be342fd703d01b623190ec528e06
Nicholas Palermo <npalermo10@gmail.com>
2017-02-01 09:08:00 -0500

    change names of classes,funcs,vars

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from math import sin,pi
 import numpy as np
 import cv2
 
-class Wave():
+class Plaid():
     def __init__(self):
         self.resolution = 1000 # lower resolution if images taking too long to load
         self.square_length = int(sqrt(2*self.resolution**2))
-        self.clear_function()
+        self.clear_plaid()
         
     def add_sinusoid(self, frequency, orientation = 0):  ## frequency is in cycles/cm. Orientation is in degrees ccw
         x = np.linspace(0, 2*pi*5, self.square_length)
@@ -18,7 +18,7 @@ class Wave():
         M = cv2.getRotationMatrix2D((cols/2,rows/2),orientation,1)
         self.cmp_wav_func += cv2.warpAffine(img,M,(cols,rows))
         
-    def clear_function(self):
+    def clear_plaid(self):
         self.cmp_wav_func = np.zeros([self.square_length, self.square_length])
         
     def show_img(self):
@@ -26,5 +26,5 @@ class Wave():
 
 w = Wave()
 w.add_sinusoid(0.5, 90)
-w.add_sinusoid(3, 0)
+w.add_sinusoid(2, 0)
 w.show_img()
